69541dfc684e4f37c2848f368c5bf574ea9934b7
Umsal96 <hongeel96@gmail.com>
2023-10-04 05:52:50 +0000

    현재 날짜를 기준으로 일정이 나오게 수정

--- html/schedule/getScheduleList.php
+++ html/schedule/getScheduleList.php
@@ -7,13 +7,25 @@
     // 데이터 베스 연결
     include '../Utility/db_connect.php';
 
-    $id = $_GET['id'];
-
     // 모듈화된 db 연결
     $conn = connectToDatabase();
 
-    $stmt = $conn->prepare("SELECT * FROM meeting_schedule_table WHERE meeting_seq = :meeting_seq");
+    $id = $_GET['id']; // 받아온 모임 고유 아이디를 변수에 넣음
+
+    // 현재 날짜와 시간을 구합니다
+    $currentDateTime = new DateTime();
+
+    // 30분을 추가한 시간을 계산합니다.
+    $currentDateTime->add(new DateInterval('PT30M'));
+
+    // 현재 날짜와 시간을 문자열로 변환합니다.
+    $currentDateTimeStr = $currentDateTime->format('Y-m-d H:i:s');
+
+    // 날짜와 시간을 합처서 현 시간을 비교하는 쿼리
+    $stmt = $conn->prepare("SELECT * FROM meeting_schedule_table 
+        WHERE meeting_seq = :meeting_seq AND CONCAT(schedule_date, ' ', schedule_time) > :current_datetime");
     $stmt->bindParam(':meeting_seq', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':current_datetime', $currentDateTimeStr, PDO::PARAM_STR);
     $stmt->execute();
 
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
